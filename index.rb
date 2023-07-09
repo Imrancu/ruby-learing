9fb41798b6f0bfd86eb3634eea138e2279829012
Imrancu <mdimran3772@gmail.com>
2023-07-09 21:18:41 +0600

    learning symbol and string

--- index.rb
+++ index.rb
@@ -1,1 +1,51 @@
-puts "Hello World"
+# my_array = [ "Apple", "Hi", 3.1242, true, 56 ]
+
+# # printing all elements of the array
+# my_array.each do |x| 
+#  p (x)
+# end
+
+# Fruits_hash = { "Apple" => 10, "Banana" => 20, "Kiwi" => 30 } 
+# Fruits_hash.each do |key, value| 
+#  print "Key: ", key, " | Value: ", value, "\n"
+# end
+
+# num_1 = 10
+# num_2 = 20
+# addition = num_1 + num_2
+# puts addition
+
+# Module Operator / 
+# puts 15 / 4
+
+# Remainder Operator %
+# puts 15 % 4
+
+# puts 15.remainder(4)
+
+# puts 15.divmod(4)
+
+# Data types Conversion
+# num_1 = '50'.to_i # string to integer
+# num_2 = 10.to_s # integer to string
+# num_3 = '10.8787'.to_f # integer to float
+
+# puts num_3.class #check data type using .class
+# p num_3
+
+
+# puts "Enter your name"
+# name = gets
+
+# puts "Enter your age"
+# age = gets
+# puts "Hello " + name + "Your are " + age + "years old"
+
+# puts "Hi there".is_a?(Object)
+# puts :Imran.is_a?(Object)
+
+puts "Hi there".object_id
+puts "Hi there".object_id
+
+puts :Imran.object_id
+puts :Imran.object_id
